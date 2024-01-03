8f522c56ed397276775267fb5b2629bb9ce97d31
tamaasn <43458726+tamaasn@users.noreply.github.com>
2024-01-03 07:50:52 +0700

    Update linkedlist.c

--- linkedlist.c
+++ linkedlist.c
@@ -7,6 +7,8 @@ struct node{
 };
 
 struct node *create_node(int data){
+  // create a node
+  
   struct node *newnode = (struct node*)malloc(sizeof(struct node*));
   newnode->x = data;
   newnode->next = NULL;
@@ -14,6 +16,8 @@ struct node *create_node(int data){
 }
 
 void insert_data(struct node *head , int data){
+  // insert data into node
+  
   struct node *newnode = (struct node*)malloc(sizeof(struct node*));
   newnode->x = data;
   newnode->next = NULL;
@@ -21,11 +25,15 @@ void insert_data(struct node *head , int data){
 }
 
 void print_nodes(struct node *head , int total){
+  // print all nodes
+  
   struct node *newnode = head;
   for(int i=0;i<total;i++){
     printf("%d\n" , newnode->x);
     newnode = newnode->next;
   }
+
+  // free all allocated memory in order to avoid memory leak
   newnode = head;
   for(int i=0;i<total;i++){
     struct node *nextnode = newnode->next;
@@ -37,11 +45,13 @@ int main(){
   int total=1;
   struct node* head = create_node(10);
   struct node* old_node = head;
+  
   for(int i=0;i<12;i++){
     total++;
     insert_data(old_node , i);
     old_node = old_node->next;
   }
+  
   print_nodes(head , total);
   return 0;
 }
